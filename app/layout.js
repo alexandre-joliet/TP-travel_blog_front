e5ce7f0eaca875f989bf76ad33b88bdcd6ed82ac
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-03 14:25:29 +0100

    :art: Format code

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,21 @@
-import Link from "next/link"
-import './globals.css'
-import Footer from "./Components/Footer/Footer"
-import Header from "./Components/Header/Header"
+import Link from "next/link";
+import "./globals.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 export const metadata = {
-  title: 'O Voyages',
-  description: 'Blog de voyages',
-}
+  title: "O Voyages",
+  description: "Blog de voyages",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-      <Header/>
-      {children}
-     <Footer/>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
