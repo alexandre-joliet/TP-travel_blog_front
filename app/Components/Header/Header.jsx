ddfb8edc7913e0fffd0bbacf63673e7499cd57db
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-03 12:18:42 +0100

    :lipstick: Update header style + classes

--- app/Components/Header/Header.jsx
+++ app/Components/Header/Header.jsx
@@ -1,35 +1,47 @@
-"use client"
+"use client";
 
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
+import styles from "./page.module.css";
 
-export default function Header ()  {
-    return (
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <img
+          className={styles.header__logo}
+          src="/images/BeachLogo.png"
+          alt="logo"
+        ></img>
+      </Link>
 
-        <header className={styles.header}>
-            
-            <Link href='/'>
-            <img className={styles.img_logo}src ="/images/BeachLogo.png" alt="logo"></img>
-            </Link>
+      <nav className={styles.header__nav}>
+        <div className={styles.nav__menu}>
+          <Link className={styles.nav__link} href="/">
+            Accueil
+          </Link>
+          <Link className={styles.nav__link} href="/Categories">
+            Catégories
+          </Link>
+          <Link className={styles.nav__link} href="/Articles">
+            Articles
+          </Link>
+          {/* <Link href='/Admin/Create'>Création</Link>*/}
+          {/*<Link href='/Admin/Users'>Gestion des utilisateurs</Link>*/}
+          <Link className={styles.nav__link} href="/Favorites">
+            Favoris
+          </Link>
+        </div>
 
-            <nav className={styles.nav}>
-                <div className={styles.menu}>
-                    <Link className={styles.link} href='/'>Accueil</Link>
-                    <Link className={styles.link} href='/Categories'>Catégories</Link>
-                    <Link className={styles.link} href='/Articles'>Articles</Link>
-                    {/* <Link href='/Admin/Create'>Création</Link>*/}
-                    {/*<Link href='/Admin/Users'>Gestion des utilisateurs</Link>*/}
-                    <Link className={styles.link} href='/Favorites'>Favoris</Link>
-                </div>
-
-                <div className={styles.account}>
-                    <Link className= {styles.link}href='/Login'>Connexion / Inscription</Link>
-                    <Link className={styles.link}href='/About'>À propos</Link>
-                     {/*<Link href='/Account'>Mon Compte</Link>*/}
-                </div>
-            </nav>
-
-        </header>
-    )
+        <div className={styles.nav__account}>
+          <Link className={styles.nav__link} href="/Login">
+            Connexion / Inscription
+          </Link>
+          <Link className={styles.nav__link} href="/About">
+            À propos
+          </Link>
+          {/*<Link href='/Account'>Mon Compte</Link>*/}
+        </div>
+      </nav>
+    </header>
+  );
 }
