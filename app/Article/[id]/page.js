15e0fcfeefa9e5b48845512b50da0f377f10a683
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-04 12:36:14 +0100

    :lipstick: Update style + semantic

--- app/Article/[id]/page.js
+++ app/Article/[id]/page.js
@@ -1,12 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import React from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
-//import { format } from "date-fns";
+import dayjs from "dayjs";
 
-const  Article = ({data, params}) => {
+const Article = ({ data, params }) => {
   const [myData, setMyData] = useState([]);
+  const [date, setDate] = useState([]);
+
+  let convertedDate = dayjs(`${myData.created_date}`).format("DD/MM/YY");
+  console.log(convertedDate);
 
   useEffect(() => {
     fetch(`https://api-oblog2.onrender.com/Article/${params.id}`)
@@ -18,37 +23,31 @@ const  Article = ({data, params}) => {
       .catch((error) => console.error(error));
   }, [params.id]);
 
-  {
-    /* function convertDate(create_date) {
-    return format(new Date(create_date), "dd-mm-yyyy");
-  }
-const convertedDate = convertDate(dateFromDb); */
-  }
-
   return (
-    <>
-      <div className={styles.div}>
-        <main>
-        <div className={styles.article}>
-          {/*    {myData &&
-              myData.map((result) => <Article key={result.id} data={result} />)}*/}
-            <img
-              className={styles.img}
-              src={myData.image}
-              alt="photo de paysage"
-            ></img>
+    <main className={styles.main}>
+      <article className={styles.article}>
+        <img
+          className={styles.article__image}
+          src={myData.image}
+          alt="photo de paysage"
+        ></img>
+        <h3 className={styles.article__category}>{myData.label}</h3>
 
-            <h1 className={styles.title}>{myData.title}</h1>
-            <h3 className={styles.auteur}>{myData.pseudo}</h3>
-            <h3 className={styles.date}>{myData.create_date}</h3>
-            <h3 className={styles.category}>{myData.categorie}</h3>
-            <p className={styles.contenent}>{myData.content}</p>
-          </div>
-         
-        </main>
-      </div>
-    </>
+        <h1 className={styles.article__title}>{myData.title}</h1>
+        <div className={styles.article__auteur_date}>
+          <img
+            className={styles.auteur__img}
+            src="../images/john.jpg"
+            alt="Avatar de l'auteur"
+          ></img>
+          <p className={styles.article__auteur}>{myData.pseudo}</p>
+          <span className={styles.auteur_date__span}>-</span>
+          <p className={styles.article__date}>{convertedDate}</p>
+        </div>
+        <p className={styles.article__text}>{myData.content}</p>
+      </article>
+    </main>
   );
-}
+};
 
 export default Article;
