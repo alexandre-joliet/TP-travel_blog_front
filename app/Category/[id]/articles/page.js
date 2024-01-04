e63536fe5b0d4cbb3946e6ca872ea6baf77620a5
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-04 15:49:30 +0100

    :lipstick: Update style + semantic + add fetch

--- app/Category/[id]/articles/page.js
+++ app/Category/[id]/articles/page.js
@@ -1,46 +1,42 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import styles from "./page.module.css";
 import Article from "../../../Components/Article/Article";
 import { useEffect, useState } from "react";
 
 const Category = ({ data, params }) => {
   const [myData, setMyData] = useState([]);
-  console.log(params);
+  const [label, setLabel] = useState("");
+
+  useEffect(() => {
+    fetch(`https://api-oblog2.onrender.com/Category/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLabel(data.label);
+      })
+      .catch((error) => console.error(error));
+  }, [params.id]);
+
   useEffect(() => {
     fetch(`https://api-oblog2.onrender.com/Category/${params.id}/articles`)
-    
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setMyData(data);
       })
-
       .catch((error) => console.error(error));
   }, [params.id]);
+
   return (
-
-  
-    <>
-      <div className={styles.div}>
-        <main>
-          <h1 className={styles.title}>{myData.label}</h1>
-
-
-          {/*<label for='site-search'></label>
-   <input type='search' id='site-search'name='q'/>
-   <button>Recherche</button>*/}
-
-          <div className={styles.articles}>
-          {myData &&
-              myData.map((result) => <Article key={result.id} data={result} />)}
-          </div>
-        </main>
+    <main className={styles.main}>
+      <h1 className={styles.main__title}>{label}</h1>
+      <div className={styles.main__articles}>
+        {myData &&
+          myData.map((result) => <Article key={result.id} data={result} />)}
       </div>
-    </>
+    </main>
   );
-  
 };
 
 export default Category;
