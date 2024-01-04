ae0fb3c0db7752fe6b5978ff2d2412e8aee19dd8
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-04 17:21:55 +0100

    :lipstick: Update style + semantic

--- app/Login/page.js
+++ app/Login/page.js
@@ -13,7 +13,7 @@ const Login = () => {
   const [role_id, setRole_id] = useState("2");
   const [pseudo, setPseudo] = useState("");
 
-/*   const click_Login = (e) => {
+  /*   const click_Login = (e) => {
     e.preventDefault();
    
       .then((response) => response.json())
@@ -29,15 +29,14 @@ const Login = () => {
   const [responseTest, setResponseTest] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     await fetch("https://api-oblog2.onrender.com/signup", {
       method: "POST",
       host: "api-oblog2.onrender.com",
       headers: {
-        "Content-Type":"application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `last_name=${lastName}&first_name=${firstName}&avatar=&mail=${mail}&pseudo=${pseudo}N&password=${password}&role_id=${role_id}`
-        
+      body: `last_name=${lastName}&first_name=${firstName}&avatar=&mail=${mail}&pseudo=${pseudo}N&password=${password}&role_id=${role_id}`,
 
       // body: JSON.stringify({
       //   last_name: lastName,
@@ -48,116 +47,114 @@ const Login = () => {
       //   password: password,
       //   role_id: role_id,
     })
-    .then(response => {
-      console.log(response)
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP: ${response.status}`);
-      }
-     
-      
-      setResponseTest("OK");
-      return {responseOk};
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP: ${response.status}`);
+        }
 
-      
-      
-      // Utilisez text() au lieu de json() pour gérer les réponses vides
-    })
-    // .then(text => {
-    //   const data = text ? JSON.parse(text) : {};  // Convertissez en JSON seulement si le texte n'est pas vide
-    //   if (data === true) {
-    //     window.location.href = "/"; // Remplacez par l'URL de votre choix
-    //   } else {
-    //     window.location.href = "page_erreur.html";
-    //   }
-    // })
-    .catch(error => {
-      console.log(error)
-      console.error('Il y a eu un problème avec l\'opération fetch: ', error);
-      // window.location.href = "page_erreur.html";
-    });
-  }
+        setResponseTest("OK");
+        return { responseOk };
+
+        // Utilisez text() au lieu de json() pour gérer les réponses vides
+      })
+      // .then(text => {
+      //   const data = text ? JSON.parse(text) : {};  // Convertissez en JSON seulement si le texte n'est pas vide
+      //   if (data === true) {
+      //     window.location.href = "/"; // Remplacez par l'URL de votre choix
+      //   } else {
+      //     window.location.href = "page_erreur.html";
+      //   }
+      // })
+      .catch((error) => {
+        console.log(error);
+        console.error("Il y a eu un problème avec l'opération fetch: ", error);
+        // window.location.href = "page_erreur.html";
+      });
+  };
 
   return (
-    <>
-      <div className={styles.background}>
-        <main className={styles.main}>
-          <h2 className={styles.connexion}>Connexion</h2>
-          <div className={styles.connexion_form}>
-            <form className={styles.connex_form}>
-              <label className={styles.pseudo}>Pseudo : </label>
-              <input
-                type="text"
-                placeholder="  Entrer le pseudo"
-                className={styles.username}
-                required
-              />
-              <label>Mot de passe : </label>
-              <input
-                type="password"
-                placeholder="  Entrer le mot de passe"
-                className={styles.password}
-                required
-              />
-              <div className={styles.submit}>
-                <input type="submit" value="Connexion" />
-              </div>
-            </form>
-          </div>
-
-          <h2 className={styles.inscription}>S&apos;inscrire</h2>
-          <div className={styles.inscription_form}>
-            <form  onSubmit={handleSubmit} className={styles.inscrip_form}>
-              <label htmlFor="lastname">Nom :</label>
-              <input
-                type="text"
-                placeholder="  Entrer le nom"
-                className={styles.lastname}
-                value={lastName}
-                onChange={(e)=>setLastName(e.target.value)}
-              />
-              <label htmlFor="firstname">Prénom :</label>{" "}
-              <input
-                type="text"
-                placeholder="  Entrer le prénom"
-                className={styles.firstname}
-                value={firstName}
-                onChange={(e)=>setFirstName(e.target.value)}
-              />
-              <label htmlFor="pseudo">Pseudo :</label>
-              <input
-                type="text"
-                placeholder="  Entrer le pseudo"
-                className={styles.pseudo}
-                value={pseudo}
-                onChange={(e)=>setPseudo(e.target.value)}
-              />
-              <label htmlFor="email">Email :</label>
-              <input
-                type="text"
-                placeholder="  Entrer l'email"
-                className={styles.email}
-                value={mail}
-                onChange={(e)=>setMail(e.target.value)}
-              />
-              <label htmlFor="password">Mot de passe :</label>
-              <input
-                type="password"
-                placeholder="  Entrer le mot de passe"
-                className={styles.pass}
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-              />
-              <div className={styles.submit2}>
-                <input type="submit" value="S'inscrire" />
-               
-              </div>
-            </form>
-
-            { responseTest === "OK" && (<div className={styles.message}>Le compte a bien été créé !</div>)}
-          </div>
-        </main>
+    <main className={styles.main}>
+      <div className={styles.signin__container}>
+        <h2 className={styles.signin__title}>Connexion</h2>
+        <form className={styles.signin__form}>
+          <label className={styles.signin__form_text}>Adresse mail :</label>
+          <input
+            type="text"
+            placeholder="Entrez l'adresse mail"
+            className={styles.signin__form_input}
+            required
+          />
+          <label className={styles.signin__form_text}>Mot de passe :</label>
+          <input
+            type="password"
+            placeholder="Entrez le mot de passe"
+            className={styles.signin__form_input}
+            required
+          />
+          <input
+            type="submit"
+            value="Valider"
+            className={styles.signin__form_submit}
+          />
+        </form>
       </div>
-    </>
+
+      <div className={styles.signup__container}>
+        <h2 className={styles.signup__title}>S&apos;inscrire</h2>
+        <form onSubmit={handleSubmit} className={styles.signup__form}>
+          <label htmlFor="lastname" className={styles.signup__form_text}>Nom :</label>
+          <input
+            type="text"
+            placeholder="Entrez votre nom"
+            className={styles.signup__form_input}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label htmlFor="firstname" className={styles.signup__form_text}>Prénom :</label>
+          <input
+            type="text"
+            placeholder="Entrez votre prénom"
+            className={styles.signup__form_input}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <label htmlFor="pseudo" className={styles.signup__form_text}>Pseudo :</label>
+          <input
+            type="text"
+            placeholder="Entrez votre pseudo"
+            className={styles.signup__form_input}
+            value={pseudo}
+            onChange={(e) => setPseudo(e.target.value)}
+          />
+          <label htmlFor="email" className={styles.signup__form_text}>Email :</label>
+          <input
+            type="text"
+            placeholder="Entrez votre adresse mail"
+            className={styles.signup__form_input}
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <label htmlFor="password" className={styles.signup__form_text}>Mot de passe :</label>
+          <input
+            type="password"
+            placeholder="Entrez votre mot de passe"
+            className={styles.signup__form_input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="S'inscrire"
+            className={styles.signup__form_submit}
+          />
+        </form>
+
+        {responseTest === "OK" && (
+          <div className={styles.message}>Le compte a bien été créé !</div>
+        )}
+      </div>
+    </main>
   );
 };
 
