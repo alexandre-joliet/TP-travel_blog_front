c192c33178717a4f0fadf90c391404d09807935d
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-08 17:13:04 +0100

    :art: Improve validation message

--- app/Login/page.js
+++ app/Login/page.js
@@ -13,22 +13,10 @@ const Login = () => {
   const [role_id, setRole_id] = useState("2");
   const [pseudo, setPseudo] = useState("");
 
-  /*   const click_Login = (e) => {
-    e.preventDefault();
-   
-      .then((response) => response.json())
-      .then((data) => {
-        if (resourceLimits.message === "SUCCESS") {
-          alert("Vous êtes connecté.");
-          this.goToMain();
-        } else {
-          alert("Vérifiez vos informations, s'il-vous-plait.");
-        }
-      });
-  }; */
-  const [responseTest, setResponseTest] = useState("");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const [createAccountValidation, setCreateAccountValidation] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
     await fetch("https://api-travel-blog.onrender.com/signup", {
       method: "POST",
@@ -37,39 +25,23 @@ const Login = () => {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: `last_name=${lastName}&first_name=${firstName}&avatar=&mail=${mail}&pseudo=${pseudo}N&password=${password}&role_id=${role_id}`,
-
-      // body: JSON.stringify({
-      //   last_name: lastName,
-      //   first_name: firstName,
-      //   avatar: "",
-      //   mail: mail,
-      //   pseudo: pseudo,
-      //   password: password,
-      //   role_id: role_id,
     })
       .then((response) => {
         console.log(response);
+
         if (!response.ok) {
           throw new Error(`Erreur HTTP: ${response.status}`);
         }
 
-        setResponseTest("OK");
-        return { responseOk };
-
-        // Utilisez text() au lieu de json() pour gérer les réponses vides
+        setCreateAccountValidation("OK");
+        return { response };
       })
-      // .then(text => {
-      //   const data = text ? JSON.parse(text) : {};  // Convertissez en JSON seulement si le texte n'est pas vide
-      //   if (data === true) {
-      //     window.location.href = "/"; // Remplacez par l'URL de votre choix
-      //   } else {
-      //     window.location.href = "page_erreur.html";
-      //   }
-      // })
       .catch((error) => {
         console.log(error);
-        console.error("Il y a eu un problème avec l'opération fetch: ", error);
-        // window.location.href = "page_erreur.html";
+        console.error(
+          "Un problème est survenu lors de la création de compte : ",
+          error
+        );
       });
   };
 
@@ -103,7 +75,9 @@ const Login = () => {
       <div className={styles.signup__container}>
         <h2 className={styles.signup__title}>S&apos;inscrire</h2>
         <form onSubmit={handleSubmit} className={styles.signup__form}>
-          <label htmlFor="lastname" className={styles.signup__form_text}>Nom :</label>
+          <label htmlFor="lastname" className={styles.signup__form_text}>
+            Nom :
+          </label>
           <input
             type="text"
             placeholder="Entrez votre nom"
@@ -111,7 +85,9 @@ const Login = () => {
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
-          <label htmlFor="firstname" className={styles.signup__form_text}>Prénom :</label>
+          <label htmlFor="firstname" className={styles.signup__form_text}>
+            Prénom :
+          </label>
           <input
             type="text"
             placeholder="Entrez votre prénom"
@@ -119,7 +95,9 @@ const Login = () => {
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
-          <label htmlFor="pseudo" className={styles.signup__form_text}>Pseudo :</label>
+          <label htmlFor="pseudo" className={styles.signup__form_text}>
+            Pseudo :
+          </label>
           <input
             type="text"
             placeholder="Entrez votre pseudo"
@@ -127,7 +105,9 @@ const Login = () => {
             value={pseudo}
             onChange={(e) => setPseudo(e.target.value)}
           />
-          <label htmlFor="email" className={styles.signup__form_text}>Email :</label>
+          <label htmlFor="email" className={styles.signup__form_text}>
+            Email :
+          </label>
           <input
             type="text"
             placeholder="Entrez votre adresse mail"
@@ -135,7 +115,9 @@ const Login = () => {
             value={mail}
             onChange={(e) => setMail(e.target.value)}
           />
-          <label htmlFor="password" className={styles.signup__form_text}>Mot de passe :</label>
+          <label htmlFor="password" className={styles.signup__form_text}>
+            Mot de passe :
+          </label>
           <input
             type="password"
             placeholder="Entrez votre mot de passe"
@@ -150,8 +132,16 @@ const Login = () => {
           />
         </form>
 
-        {responseTest === "OK" && (
-          <div className={styles.message}>Le compte a bien été créé !</div>
+        <div className={styles.signup__validationMessage}>
+          <p>Le compte a bien été créé.</p>
+          <p>Vous pouvez dès à présent vous connecter &#127881; !</p>
+        </div>
+
+        {createAccountValidation === "OK" && (
+          <div className={styles.signup__validationMessage}>
+            <p>Le compte a bien été créé.</p>
+            <p>Vous pouvez dès à présent vous connecter &#127881; !</p>
+          </div>
         )}
       </div>
     </main>
