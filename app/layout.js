d2b4628ab3db397725c27d95673983f295f7fa33
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-10 18:29:24 +0100

    :art: Update metadata

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 
 export const metadata = {
-  title: "O Voyages",
-  description: "Blog de voyages",
+  title: "O'Voyages",
+  description: "Bienvenue sur mon blog, où vous trouverez ici une passerelle vers mes aventures, mes expériences uniques et mes destinations favorites. Que vous soyez un voyageur aguerri ou un explorateur en herbe, ce blog est votre guide pour plonger avec moi au cœur de destinations fascinantes à travers le globe.",
 };
 
 export default function RootLayout({ children }) {
