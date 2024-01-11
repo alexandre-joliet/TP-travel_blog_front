196a79d8098d163dac33b4c8ec0a5c85c1864fd1
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-11 18:02:37 +0100

    :construction: Update login

--- app/Login/page.js
+++ app/Login/page.js
@@ -3,19 +3,49 @@ import React from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
-  const [myData, setMyData] = useState([]);
   const [lastName, setLastName] = useState("");
   const [firstName, setFirstName] = useState("");
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [role_id, setRole_id] = useState("2");
   const [pseudo, setPseudo] = useState("");
+  const [createAccountValidation, setCreateAccountValidation] = useState(false);
 
-  const [createAccountValidation, setCreateAccountValidation] = useState("");
+  const [mailLogin, setMailLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
 
-  const handleSubmit = async (event) => {
+  const router = useRouter();
+
+  const handleSubmitLogin = async (event) => {
+    event.preventDefault();
+
+    // TODO: A repasser sur la bonne URL en prod
+    await fetch("http://localhost:3000/login", { //https://api-travel-blog.onrender.com/login
+      method: "POST",
+      host: "api-travel-blog.onrender.com",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `mail=${mailLogin}&password=${passwordLogin}`,
+      credentials: "include"
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        router.push('/');
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        console.error("Un problème est survenu lors de la connexion : ", error);
+      });
+  };
+
+  const handleSubmitSignup = async (event) => {
     event.preventDefault();
 
     await fetch("https://api-travel-blog.onrender.com/signup", {
@@ -33,7 +63,7 @@ const Login = () => {
           throw new Error(`Erreur HTTP: ${response.status}`);
         }
 
-        setCreateAccountValidation("OK");
+        setCreateAccountValidation(true);
         return { response };
       })
       .catch((error) => {
@@ -49,12 +79,14 @@ const Login = () => {
     <main className={styles.main}>
       <div className={styles.signin__container}>
         <h2 className={styles.signin__title}>Connexion</h2>
-        <form className={styles.signin__form}>
+        <form onSubmit={handleSubmitLogin} className={styles.signin__form}>
           <label className={styles.signin__form_text}>Adresse mail :</label>
           <input
             type="text"
             placeholder="Entrez l'adresse mail"
             className={styles.signin__form_input}
+            value={mailLogin}
+            onChange={(event) => setMailLogin(event.target.value)}
             required
           />
           <label className={styles.signin__form_text}>Mot de passe :</label>
@@ -62,6 +94,8 @@ const Login = () => {
             type="password"
             placeholder="Entrez le mot de passe"
             className={styles.signin__form_input}
+            value={passwordLogin}
+            onChange={(event) => setPasswordLogin(event.target.value)}
             required
           />
           <input
@@ -74,7 +108,7 @@ const Login = () => {
 
       <div className={styles.signup__container}>
         <h2 className={styles.signup__title}>S&apos;inscrire</h2>
-        <form onSubmit={handleSubmit} className={styles.signup__form}>
+        <form onSubmit={handleSubmitSignup} className={styles.signup__form}>
           <label htmlFor="lastname" className={styles.signup__form_text}>
             Nom :
           </label>
@@ -83,7 +117,7 @@ const Login = () => {
             placeholder="Entrez votre nom"
             className={styles.signup__form_input}
             value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(event) => setLastName(event.target.value)}
           />
           <label htmlFor="firstname" className={styles.signup__form_text}>
             Prénom :
@@ -93,7 +127,7 @@ const Login = () => {
             placeholder="Entrez votre prénom"
             className={styles.signup__form_input}
             value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={(event) => setFirstName(event.target.value)}
           />
           <label htmlFor="pseudo" className={styles.signup__form_text}>
             Pseudo :
@@ -103,7 +137,7 @@ const Login = () => {
             placeholder="Entrez votre pseudo"
             className={styles.signup__form_input}
             value={pseudo}
-            onChange={(e) => setPseudo(e.target.value)}
+            onChange={(event) => setPseudo(event.target.value)}
           />
           <label htmlFor="email" className={styles.signup__form_text}>
             Email :
@@ -113,7 +147,7 @@ const Login = () => {
             placeholder="Entrez votre adresse mail"
             className={styles.signup__form_input}
             value={mail}
-            onChange={(e) => setMail(e.target.value)}
+            onChange={(event) => setMail(event.target.value)}
           />
           <label htmlFor="password" className={styles.signup__form_text}>
             Mot de passe :
@@ -123,7 +157,7 @@ const Login = () => {
             placeholder="Entrez votre mot de passe"
             className={styles.signup__form_input}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(event) => setPassword(event.target.value)}
           />
           <input
             type="submit"
@@ -132,12 +166,7 @@ const Login = () => {
           />
         </form>
 
-        <div className={styles.signup__validationMessage}>
-          <p>Le compte a bien été créé.</p>
-          <p>Vous pouvez dès à présent vous connecter &#127881; !</p>
-        </div>
-
-        {createAccountValidation === "OK" && (
+        {createAccountValidation === true && (
           <div className={styles.signup__validationMessage}>
             <p>Le compte a bien été créé.</p>
             <p>Vous pouvez dès à présent vous connecter &#127881; !</p>
