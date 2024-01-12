9d42e8df21c08de8b550f199760b8338b07c2035
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-12 21:42:38 +0100

    :sparkles: Article creation + deletion functional

--- app/Admin/ArticleManagement/page.js
+++ app/Admin/ArticleManagement/page.js
@@ -51,7 +51,41 @@ const ArticleManagement = () => {
   //   );
   // }
 
-  // TODO: URL à adapter
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newContent, setNewContent] = useState("");
+  const [newCategory, setNewCategory] = useState("");
+  const [newImage, setNewImage] = useState("");
+
+  /**
+   * @param {Event} event
+   */
+  const handleCreateArticle = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("https://api-travel-blog.onrender.com/article", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `title=${newTitle}&description=${newDescription}&content=${newContent}&image=${newImage}&category=${newCategory}`,
+      });
+    } catch (error) {
+      console.error("Erreur lors de la création de l'article : ", error);
+    } finally {
+      const responseGet = await fetch("https://api-travel-blog.onrender.com/articles");
+      const updatedArticlesList = await responseGet.json();
+      setAllArticles(updatedArticlesList);
+    }
+
+    setNewTitle("");
+    setNewDescription("");
+    setNewContent("");
+    setNewCategory("");
+    setNewImage("");
+  };
+
   const handleDeleteArticle = async (id) => {
     try {
       const responseDelete = await fetch(
@@ -78,7 +112,7 @@ const ArticleManagement = () => {
     setSearchedText(value);
   };
 
-  const filteredArticles = articles.filter((item) => {
+  const filteredArticles = allArticles.filter((item) => {
     return item.description.toLowerCase().includes(searchedText.toLowerCase());
   });
 
@@ -98,8 +132,8 @@ const ArticleManagement = () => {
                 <input
                   type="text"
                   placeholder="Titre de l'article"
-                  onChange=""
-                  value=""
+                  onChange={(event) => setNewTitle(event.target.value)}
+                  value={newTitle}
                   id="title"
                   className={styles.creation__form_input}
                 />
@@ -116,7 +150,8 @@ const ArticleManagement = () => {
                 <textarea
                   type="textarea"
                   placeholder="Description de l'article"
-                  onChange=""
+                  onChange={(event) => setNewDescription(event.target.value)}
+                  value={newDescription}
                   id="description"
                   className={styles.creation__form_textaera}
                 />
@@ -133,8 +168,8 @@ const ArticleManagement = () => {
                 <textarea
                   type="textarea"
                   placeholder="Contenu de l'article"
-                  onChange=""
-                  value=""
+                  onChange={(event) => setNewContent(event.target.value)}
+                  value={newContent}
                   id="description"
                   className={styles.creation__form_textaera}
                 />
@@ -150,6 +185,8 @@ const ArticleManagement = () => {
                 </label>
                 <select
                   placeholder="Catégorie"
+                  onChange={(event) => setNewCategory(event.target.value)}
+                  value={newCategory}
                   id="category"
                   className={styles.creation__form_select}
                 >
@@ -169,8 +206,8 @@ const ArticleManagement = () => {
                 <input
                   type="text"
                   placeholder="URL de l'image"
-                  onChange=""
-                  value=""
+                  onChange={(event) => setNewImage(event.target.value)}
+                  value={newImage}
                   id="picture"
                   className={styles.creation__form_input}
                 />
@@ -179,7 +216,7 @@ const ArticleManagement = () => {
             <input
               type="submit"
               value="Valider"
-              onClick=""
+              onClick={handleCreateArticle}
               className={styles.creation__form_submit}
             />
           </form>
@@ -187,7 +224,6 @@ const ArticleManagement = () => {
 
         <section className={styles.articles__edition}>
           <h2>Edition et suppression des articles</h2>
-
           <input
             className={styles.search__input}
             type="search"
@@ -195,9 +231,7 @@ const ArticleManagement = () => {
             value={searchedText}
             onChange={handleChangeSearchTextInput}
           />
-
           {isLoading && <Spinner />}
-
           {filteredArticles &&
             filteredArticles.map((result) => (
               <div key={result.id}>
@@ -216,7 +250,7 @@ const ArticleManagement = () => {
                     <input
                       type="submit"
                       value="Modifier"
-                      onClick=""
+                      // onClick=""
                       className={styles.edit__form_submit}
                     />
                   </form>
@@ -228,8 +262,8 @@ const ArticleManagement = () => {
                   <img
                     onClick={() => handleDeleteArticle(result.id)}
                     src="/images/trash-bin.png"
-                    alt="Supprimer l'article'"
-                    title="Supprimer l'article'"
+                    alt="Supprimer l'article"
+                    title="Supprimer l'article"
                     className={styles.delete__button}
                   ></img>
                 </div>
